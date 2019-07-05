b88949d2f371281919278b98d7d758a43019d7a5
Alice Brunel <alc.brunel@gmail.com>
2019-07-05 18:22:39 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(phrase = "Squawk!")
+def parrot(string, phrase = "Squawk!")
   puts string
   return string
 end
